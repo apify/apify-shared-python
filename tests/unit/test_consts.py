53d27d9551ddb143ae26e33b2bb7f7e9a06871d0
Jiří Moravčík <jiri.moravcik@gmail.com>
2023-08-01 14:09:17 +0200

    feat: Use Actor env vars (#5)

--- tests/unit/test_consts.py
+++ tests/unit/test_consts.py
@@ -1,6 +1,8 @@
 import itertools
+from enum import Enum
+from typing import List
 
-from apify_shared.consts import BOOL_ENV_VARS, DATETIME_ENV_VARS, FLOAT_ENV_VARS, INTEGER_ENV_VARS, STRING_ENV_VARS, ApifyEnvVars
+from apify_shared.consts import BOOL_ENV_VARS, DATETIME_ENV_VARS, FLOAT_ENV_VARS, INTEGER_ENV_VARS, STRING_ENV_VARS, ActorEnvVars, ApifyEnvVars
 
 
 class TestConsts:
@@ -19,9 +21,16 @@ class TestConsts:
             assert not set(first) & set(second)
 
     def test_env_vars_types_defined_for_all_env_vars(self) -> None:
-        """Test that all env vars from `ApifyEnvVars` have a defined type."""
+        """Test that all env vars from `ApifyEnvVars` and `ActorEnvVars` have a defined type."""
         env_vars_from_types = set(
             list(BOOL_ENV_VARS) + list(DATETIME_ENV_VARS) + list(FLOAT_ENV_VARS) + list(INTEGER_ENV_VARS) + list(STRING_ENV_VARS),
         )
-        env_vars_from_enum = set(ApifyEnvVars)
-        assert env_vars_from_types == env_vars_from_enum
+        env_vars_from_enums = set(ApifyEnvVars).union(set(ActorEnvVars))
+        assert env_vars_from_types == env_vars_from_enums
+
+    def test_env_vars_have_correct_prefix(self) -> None:
+        """Test that all env vars from `ApifyEnvVars` and `ActorEnvVars` have the correct prefix."""
+        for (env_vars_class, prefix) in [(ActorEnvVars, 'ACTOR_'), (ApifyEnvVars, 'APIFY_')]:
+            env_vars: List[Enum] = list(env_vars_class)
+            for env_var in env_vars:
+                assert env_var.value.startswith(prefix) is True
