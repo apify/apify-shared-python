7b377acffc7560b047c61f16171dc86f4e3b76b6
František Nesveda <frantisek@apify.com>
2023-08-01 10:26:46 +0200

    Add `ignore_docs` decorator to all internal utils

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+[1.0.1](../../releases/tag/v1.0.1) - 2023-08-01
+
+Added the `ignore_docs` decorator to all internal utilities.
+
 [1.0.0](../../releases/tag/v1.0.0) - 2023-07-25
 
 Initial release of the package.
--- src/apify_shared/utils.py
+++ src/apify_shared/utils.py
@@ -12,11 +12,18 @@ ListOrDict = TypeVar('ListOrDict', List, Dict)
 T = TypeVar('T')
 
 
+def ignore_docs(method: T) -> T:
+    """Mark that a method's documentation should not be rendered. Functionally, this decorator is a noop."""
+    return method
+
+
+@ignore_docs
 def filter_out_none_values_recursively(dictionary: Dict) -> Dict:
     """Return copy of the dictionary, recursively omitting all keys for which values are None."""
     return cast(dict, _filter_out_none_values_recursively_internal(dictionary))
 
 
+@ignore_docs
 def _filter_out_none_values_recursively_internal(
     dictionary: Dict,
     remove_empty_dicts: Optional[bool] = None,
@@ -37,26 +44,25 @@ def _filter_out_none_values_recursively_internal(
     return result
 
 
-def ignore_docs(method: T) -> T:
-    """Mark that a method's documentation should not be rendered. Functionally, this decorator is a noop."""
-    return method
-
-
+@ignore_docs
 def is_content_type_json(content_type: str) -> bool:
     """Check if the given content type is JSON."""
     return bool(re.search(r'^application/json', content_type, flags=re.IGNORECASE))
 
 
+@ignore_docs
 def is_content_type_xml(content_type: str) -> bool:
     """Check if the given content type is XML."""
     return bool(re.search(r'^application/.*xml$', content_type, flags=re.IGNORECASE))
 
 
+@ignore_docs
 def is_content_type_text(content_type: str) -> bool:
     """Check if the given content type is text."""
     return bool(re.search(r'^text/', content_type, flags=re.IGNORECASE))
 
 
+@ignore_docs
 def is_file_or_bytes(value: Any) -> bool:
     """Check if the input value is a file-like object or bytes.
 
@@ -67,11 +73,13 @@ def is_file_or_bytes(value: Any) -> bool:
     return isinstance(value, (bytes, bytearray, io.IOBase))
 
 
+@ignore_docs
 def json_dumps(obj: Any) -> str:
     """Dump JSON to a string with the correct settings and serializer."""
     return json.dumps(obj, ensure_ascii=False, indent=2, default=str)
 
 
+@ignore_docs
 def maybe_extract_enum_member_value(maybe_enum_member: Any) -> Any:
     """Extract the value of an enumeration member if it is an Enum, otherwise return the original value."""
     if isinstance(maybe_enum_member, Enum):
@@ -79,6 +87,7 @@ def maybe_extract_enum_member_value(maybe_enum_member: Any) -> Any:
     return maybe_enum_member
 
 
+@ignore_docs
 def parse_date_fields(data: ListOrDict, max_depth: int = PARSE_DATE_FIELDS_MAX_DEPTH) -> ListOrDict:
     """Recursively parse date fields in a list or dictionary up to the specified depth."""
     if max_depth < 0:
@@ -98,7 +107,7 @@ def parse_date_fields(data: ListOrDict, max_depth: int = PARSE_DATE_FIELDS_MAX_D
             elif isinstance(value, dict):
                 parsed_value = parse_date_fields(value, max_depth - 1)
             elif isinstance(value, list):
-                parsed_value = parse_date_fields(value, max_depth)
+                parsed_value = parse_date_fields(value, max_depth)  # type: ignore # mypy doesn't work with decorators and recursive calls well
             return parsed_value
 
         return {key: parse(key, value) for (key, value) in data.items()}
